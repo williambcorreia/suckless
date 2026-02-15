dbb443ed04983d58d4518df9c6bbadc91935175e
William <wburgard6@gmail.com>
2026-02-15 00:21:52 -0300

    ok

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -3,7 +3,7 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "/home/williamc/documentos/suckless/scripts/blocks/wifi.sh",	3,		0},
 	{"", "/home/williamc/documentos/suckless/scripts/blocks/bat.sh",	3,		0},
-	{"", "/home/williamc/documentos/suckless/scripts/blocks/volume.sh",     1,		0},
+	{"", "/home/williamc/documentos/suckless/scripts/blocks/volume.sh",     3600,		5},
 	{"", "/home/williamc/documentos/suckless/scripts/blocks/date.sh",	30,		0},
 };
 
