ca348839b6020d8011d0edc770073a66ff0cd656
William Correia <wburgard6@gmail.com>
2026-03-16 19:16:18 -0300

    script de bateria alterado

--- scripts/blocks/bat.sh
+++ scripts/blocks/bat.sh
@@ -2,18 +2,23 @@
 
 bat=$(acpi -b | awk '/Battery/ { print $4 }' | sed s/,//g | tr -d '%')
 car=$(acpi -b | awk '/Battery/ { print $3 }' | sed s/,//g)
+num=$(acpi -b | awk '/Battery/ { print $5 }')
 
-case 1 in
-	$((bat <= 20)) )		icon="󰁻 $bat%";;
-	$((bat <= 50)) )		icon="󰁾 $bat%";;
-	$((bat <= 70)) )		icon="󰂀 $bat%";;
-	$((bat <= 90)) )		icon="󰂁 $bat%";;
-	*)				icon="󰁹 $bat%";;
-esac
+if [ "$bat" = "charging" ]; then
+	icon="󰂄 $num"	
+else
+	case 1 in
+		$((bat <= 20)) )		icon="󰁻 $bat%";;
+		$((bat <= 50)) )		icon="󰁾 $bat%";;
+		$((bat <= 70)) )		icon="󰂀 $bat%";;
+		$((bat <= 90)) )		icon="󰂁 $bat%";;
+		*)				icon="󰁹 $bat%";;
+	esac
+fi
 
 if [ "$car" = "Charging" ]; then
 	icon="󰂄 $bat%"
 	echo "$icon"
-else
-	echo "$icon"
 fi
+
+echo "$icon"
