fa906b640e1b395849c9a2d5f394cff980e59798
William Correia <wburgard6@gmail.com>
2026-03-16 22:55:35 -0300

    add brightness block

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -2,6 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "/home/williamc/documentos/suckless/scripts/blocks/wifi.sh",	3,		0},
+	{"", "/home/williamc/documentos/suckless/scripts/blocks/bright.sh",     0,              6},
 	{"", "/home/williamc/documentos/suckless/scripts/blocks/bat.sh",	3,		0},
 	{"", "/home/williamc/documentos/suckless/scripts/blocks/volume.sh",     3,		5},
 	{"", "/home/williamc/documentos/suckless/scripts/blocks/date.sh",	30,		0},
